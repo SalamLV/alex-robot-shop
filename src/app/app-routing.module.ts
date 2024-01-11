e9d97453d631537aed327545b45817c8a88c71ef
Aleksejs Demjanenko <aleksejs.demjanenko@gmail.com>
2024-01-11 19:36:53 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 const routes: Routes = [ 
   {path:"home", component: HomeComponent, title: "Alex's robot-shop"},
   {path: "catalog", component: CatalogComponent, title: "Alex's catalog"},
   {path: "cart", component: CartComponent, title: "Alex's cart"},
+  {path: "sign-in", component: SignInComponent},
+  {path: "form-controls", component: TemplateFormControlsComponent},
   {path:'', redirectTo: '/home', pathMatch: "full"}
 ];
 
